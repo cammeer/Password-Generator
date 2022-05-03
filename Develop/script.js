9294136e2b974a06ee5430bdeaa8380ef19270da
cardinal <caren@merzhaus.org>
2022-05-03 18:58:13 -0400

    troubleshooting

--- Develop/script.js
+++ Develop/script.js
@@ -1,4 +1,5 @@
 // Assignment code here
+//troubleshooting!
 
 // var charactersNestedArray = [
 //   ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"]
